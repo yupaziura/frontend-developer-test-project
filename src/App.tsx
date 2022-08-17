98bc35dd483022808d05a47de4e2da4c2b8a519a
YuliiaPaziura <yuliiapaziura@gmail.com>
2022-08-17 10:58:31 +0300

    Add logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,44 @@
-import React, { useCallback } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { View } from 'react-native'
 
 import { Project } from 'app/types/Project'
+import {Task} from 'app/types/Task'
 import { ProjectsListView } from 'app/widgets/ProjectsList'
+import { TasksListView } from './widgets/TasksList'
 import { PROJECTS } from 'app/mock/data'
 
 import styles from './App.styles'
 
 function App() {
+  const [activeProject, setActiveProject] = useState(99);
+  const [projects, setProjects] = React.useState(PROJECTS)
+
+  let fullTaskList: Task[] = [];
+    PROJECTS.forEach((project) => {
+      project.tasks.forEach((task)=> {
+          fullTaskList.push(task)
+        })
+      })
+  
+
+
   const onProjectPress = useCallback((project: Project) => {
-    // TODO
+    
+    activeProject === 99 || activeProject !== PROJECTS.indexOf(project)? setActiveProject(PROJECTS.indexOf(project)) : setActiveProject(99)
+  }, [activeProject])
+
+  const onTaskPress = useCallback((task: Task) => {
+    console.log('activeProject')
   }, [])
 
   return (
     <View style={styles.container}>
       <View style={styles.column}>
-        <ProjectsListView projects={PROJECTS} onProjectPress={onProjectPress} />
+        <ProjectsListView projects={PROJECTS} onProjectPress={onProjectPress} selectedProjectIndex={activeProject}/>
       </View>
-      <View style={styles.column}></View>
+      <View style={styles.column}>
+        <TasksListView tasks={activeProject!==99? PROJECTS[activeProject].tasks : fullTaskList}  onTaskPress={onTaskPress}/>
+      </View>
     </View>
   )
 }
